a2493717e823b66900a78d68ee1e3fe56db448d9
Andrii Mosin <mosin.react.ukrtechinfo@gmail.com>
2024-01-30 19:33:59 +0200

    task_01: fix

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,16 +1,16 @@
-import { readFile } from 'node:fs/promises'
-import { MESSAGES } from '../utils.js'
+import { readFile } from 'node:fs/promises';
+import { MESSAGES } from '../utils.js';
 
 const read = async () => {
   // Write your code here
-  const READ_FILE_NAME = './files/fileToRead2.txt'
-  const READ_FILE_NAME_URL = new URL(READ_FILE_NAME, import.meta.url)
+  const READ_FILE_NAME = './files/fileToRead.txt';
+  const READ_FILE_NAME_URL = new URL(READ_FILE_NAME, import.meta.url);
   try {
-    const contents = await readFile(READ_FILE_NAME_URL, { encoding: 'utf8' })
-    console.log(contents)
+    const contents = await readFile(READ_FILE_NAME_URL, { encoding: 'utf8' });
+    console.log(contents);
   } catch (err) {
-    throw Error(MESSAGES.error)
+    throw Error(MESSAGES.error);
   }
-}
+};
 
-await read()
+await read();
